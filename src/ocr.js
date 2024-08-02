e3432483b1c4996954515d728680b91bf3c1dc28
andrewelalfy57 <andrewabotwila@gmail.com>
2024-08-02 15:24:04 +0300

    Updates

--- src/ocr.js
+++
@@ -1,9 +0,0 @@
-// src/ocr.js
-import Tesseract from 'tesseract.js';
-
-export const performOCR = async (file) => {
-  const result = await Tesseract.recognize(file, 'eng', {
-    logger: (m) => console.log(m),
-  });
-  return result.data.text;
-};
